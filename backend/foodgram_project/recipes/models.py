595a6118fcffe5e7077fce8eb8226105c3b36884
Александр Русанов <shurik.82rusanov@yandex.ru>
2023-11-28 23:47:00 +0300

    models

--- backend/foodgram_project/recipes/models.py
+++ backend/foodgram_project/recipes/models.py
@@ -128,3 +128,32 @@ class Neo(models.Model):
 
     def __str__(self):
         return f'{self.user} добавил {self.recipe} в избранное'
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='shopping_cart', verbose_name='Пользователь'
+    )
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE,
+        related_name='shopping_cart', verbose_name='Рецепт в списке покупок'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата добавления в список покупок',
+    )
+
+    class Meta:
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
+        ordering = ('-pub_date',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_shopping_cart_user_recipe',
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} добавил {self.recipe} в список покупок'
